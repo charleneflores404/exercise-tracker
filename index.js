9ef7538c41bdc8f290f41310767ee20bc6df7c18
Charlene <charlene.flores.404@gmail.com>
2025-10-05 23:54:37 +0800

    Add POST for creating exercise

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const exerciseSchema = new Schema({
   // username: { type: String, required: true },
   description: { type: String, required: true },
   duration: { type: Number, required: true },
-  date: { type: Date, required: true }, // type string?
+  date: { type: String, required: true }, // type Date?
 });
 
 const userSchema = new Schema({
@@ -36,6 +36,8 @@ const logSchema = new Schema({
 });
 
 const User = mongoose.model("User", userSchema);
+const Exercise = mongoose.model("Exercise", exerciseSchema);
+const Log = mongoose.model("Log", logSchema);
 
 app.use(cors());
 app.use(express.static("public"));
@@ -56,6 +58,31 @@ const createUser = async (username) => {
     return newUser;
   } catch (err) {
     console.log("Error saving user", err);
+    throw err;
+  }
+};
+
+// Create exercise
+const createExercise = async (exerciseFields) => {
+  try {
+    console.log("reached1");
+    const exercise = new Exercise({ exerciseFields });
+    console.log("reached2");
+    const newExercise = await exercise.save();
+    console.log("reached3");
+  } catch (err) {
+    console.log("Error saving exercise", err);
+    throw err;
+  }
+};
+
+// Create log
+const createLog = async (logFields) => {
+  try {
+    const log = new Log({ logFields });
+  } catch (err) {
+    console.log("Error saving log", err);
+    throw err;
   }
 };
 
@@ -63,6 +90,16 @@ const findUser = async (username) => {
   try {
     const userFound = await User.findOne({ username });
     return userFound ? userFound : null;
+  } catch (err) {
+    console.log("Error finding user", err);
+    throw err;
+  }
+};
+
+const findUserbyId = async (_id) => {
+  try {
+    const userFound = await User.findById({ _id });
+    return userFound ? userFound.username : null;
   } catch (err) {
     console.log("Error finding user", err);
     throw err;
@@ -98,7 +135,39 @@ app.get("/api/users", async (req, res) => {
     const users = await User.find();
     res.json(users);
   } catch (err) {
-    console.error("Error in POST /api/users", err);
+    console.error("Error in GET /api/users", err);
     res.status(500).json({ error: "Cannot get users" });
+  }
+});
+
+app.post("/api/users/:_id/exercises", async (req, res) => {
+  const _id = req.params._id;
+  console.log(_id);
+  try {
+    const username = await findUserbyId(_id);
+    console.log("Username found:", username);
+
+    if (username) {
+      const description = req.body.description;
+      const duration = req.body.duration;
+      let d = new Date(req.body.date); // convert date
+      if (isNaN(d)) {
+        d = new Date(); // date today if none given
+      }
+      const date = d.toDateString();
+
+      const exerciseFields = { description, duration, date };
+      console.log("Exercise fields created", exerciseFields);
+      const exercise = await createExercise(exerciseFields);
+      console.log("Exercise created", exercise);
+
+      // const cnt = findLog()
+      // const count = count + 1
+      // const logFields = {username, count, _id, log: exercise}
+      // const log = await createLog(exerciseFields);
+    }
+  } catch (err) {
+    console.log("Error in POST /api/users/:_id/exercises", err);
+    res.status(500).json({ error: "Cannot add exercise" });
   }
 });
